da1fd599ed3b80a2faac6ba7e909cf265fe6e4d6
Denis Tereshchenko <dotereschenko@edu.hse.ru>
2025-11-21 11:13:17 +0300

    Bump openai dep

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         'pydantic>=2.10.6',
         'ipykernel==6.29.5',
         'pyyaml==6.0.2',
-        'openai==1.51.0',
+        'openai>=1.109.1',
         'instructor==1.5.0',
         'httpx>=0.28.1'
     ],
