ebe2de137455d5903676cef611a654a3d198d238
Denis Tereshchenko <dotereschenko@edu.hse.ru>
2025-11-21 11:10:56 +0300

    Loosen deps for pydantic/httpx

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='autopenbench',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'python-dotenv==1.0.1',
         'paramiko==3.5.0',
         'termcolor==2.4.0',
         'chardet==5.2.0',
-        'pydantic==2.9.2',
+        'pydantic>=2.10.6',
         'ipykernel==6.29.5',
         'pyyaml==6.0.2',
         'openai==1.51.0',
         'instructor==1.5.0',
-        'httpx==0.27.2'
+        'httpx>=0.28.1'
     ],
     include_package_data=True,
 )
